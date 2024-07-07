5633abc0c0e7b1ffaa7db9c2e402e130b40d9d96
xiaoxu <innocent0705@gmail.com>
2024-07-07 23:28:43 +0800

    fix: update yaml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Substitute environment variables in the nginx configuration
-envsubst '$app_env' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$APP_ENV' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start nginx
 nginx -g 'daemon off;'
